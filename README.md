66833cd20907f8e2ea37e4161b8f623c1ad7c9a9
Othneil Drew <codeguydrew@gmail.com>
2019-02-18 21:39:18 -0600

    Added pagination and basic search functionality

---
+++ README.md
@@ -0,0 +1,88 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+
+  <h3 align="center">List-Pagination-And-Filtering</h3>
+
+  <p align="center">
+    Employing progressive enhancement principles to improve the usability of a webpage using JavaScript for pagination and search filtering.
+    <br />
+    <a href="https://www.othneildrew.com/List-Pagination-And-Filtering"><strong>View Demo</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/othneildrew/List-Pagination-And-Filtering/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/othneildrew/List-Pagination-And-Filtering/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Installation](#installation)
+* [Contributing](#contributing)
+* [License](#license)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+This project is part of the Team Treehouse [Full Stack JavaScript Techdegree](https://join.teamtreehouse.com/techdegree/) program.
+
+### Built With
+
+* HTML / CSS
+* JavaScript
+
+
+
+<!-- INSTALLATION -->
+## Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/othneildrew/List-Pagination-And-Filtering.git
+```
+2. Enjoy
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Othneil Drew - [LinkedIn](https://linkedin.com/in/othneildrew) - codeguydrew@gmail.com
+
+Project Link: [https://github.com/othneildrew/List-Pagination-And-Filtering](https://github.com/othneildrew/List-Pagination-And-Filtering)
+
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+* [GitHub Pages](https://pages.github.com)
--- js/script.js
+++ js/script.js
@@ -2,49 +2,183 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+const pageContainer = document.getElementsByClassName('page')[0];
+const studentList = document.getElementsByClassName('student-list')[0];
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+let pageLimit = 10;
 
 
 
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+
+init();
+
+
+
+/***
+  * Initializes the program and sets the
+  * inital state of the application.
+  **/
+function init() {
+  let page = 1;
+
+  showPage(studentList, page);
+
+  appendPageLinks(studentList, page);
+
+  addSearchInput();
+}
+
+
+
+/***
+  * Creates a search input and appends it to the first
+  * instance of the element with a class of 'page-header'.
+  **/
+function addSearchInput() {
+  const pageHeader = document.getElementsByClassName('page-header')[0];
+
+  // Create div element with class of student-search
+  let studentSearch = document.createElement('div');
+  studentSearch.setAttribute('class', 'student-search');
+
+  // Create text input element
+  let searchInput = document.createElement('input');
+  searchInput.setAttribute('type', 'text');
+  searchInput.setAttribute('placeholder', 'Start typing to search');
+  searchInput.addEventListener('keyup', search);
+
+  // Create button element
+  let searchBtn = document.createElement('button');
+  searchBtn.textContent = 'Search';
+  searchBtn.addEventListener('click', search);
+
+  // Create search results paragraph text
+  let resultsText = document.createElement('p');
+  resultsText.style.clear = 'both';
+
+  // Append elements and place in DOM
+  studentSearch.append(searchInput);
+  //studentSearch.append(searchBtn);
+  pageHeader.append(studentSearch);
+  pageHeader.append(resultsText);
+}
+
+
+
+/***
+  * Search and return items that match search criteria
+  **/
+function search() {
+  const pageHeader = document.getElementsByClassName('page-header')[0];
+  let searchInput = document.querySelectorAll('.student-search input')[0];
+  let resultsText = document.querySelectorAll('.page-header p')[0];
+  let students = studentList.getElementsByClassName('student-item');
+  let num = 0;
+
+  for(let i = 0; i < students.length; i++) {
+
+    let studentName = students[i].querySelectorAll('.student-details h3')[0].textContent;
+    let studentEmail = students[i].querySelectorAll('.student-details span.email')[0].textContent;
+
+    // Check if student name or email match search input
+    if(studentName.match(searchInput.value) || studentEmail.match(searchInput.value)) {
+      students[i].style.display = 'block';
+      num++;
+    } else {
+      students[i].style.display = 'none';
+    }
+
+
+
+  }
+
+  resultsText.textContent = 'Showing ' + num + ' search results';
+  //resultsText.style.
+  pageHeader.append(resultsText);
+
+
+
+
+}
+
+
+
+/***
+  * Take a given 'list' of students and a given page. Loop through
+  * each instance of the items in 'list' and show based on its index.
+  **/
+function showPage(list, page) {
+  let students = list.getElementsByClassName('student-item');
+  /*
+    Display start and end calculates the range of items to
+    display based on the page limit
+  */
+  let displayStart = (page * pageLimit) - pageLimit - 1;
+  let displayEnd = page * pageLimit - 1;
+  let num = 0;
+
+  // Loop and display if within display start and end range
+  for (let i = 0; i < students.length; i++ ) {
+    if(num > displayStart && num <= displayEnd) {
+      students[i].style.display = 'block';
+    } else {
+      students[i].style.display = 'none';
+    }
+    num++;
+  }
+}
+
+
+
+/***
+  * Takes a give number of items in 'list', creates pagination links,
+  * adds it to the DOM based on the specified 'pageLimit'.
+  **/
+function appendPageLinks(list, page) {
+  const totalItems = list.childElementCount;
+  const pagesTotal = Math.ceil(totalItems / pageLimit);
+
+  // Create div element with class of pagination
+  let pageLinksContainer = document.createElement('div');
+  pageLinksContainer.setAttribute('class', 'pagination');
+
+  // Create ul element and append to div
+  let pageLinks = document.createElement('ul');
+  pageLinksContainer.append(pageLinks);
+
+  // Dynamically create buttons based on items in a given 'list'
+  for(let i = 1; i <= pagesTotal; i++) {
+    let listItem = document.createElement('li');
+    let button = document.createElement('a');
+    button.setAttribute('href', i);
+    button.textContent = i;
+
+    // Call showPage function on click event
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      // Get all pagination links and remove active class
+      let paginationLinks = document.querySelectorAll('.pagination ul li a');
+      for(let i = 0; i < pagesTotal; i++) {
+        paginationLinks[i].removeAttribute('class', 'active');
+      }
+      // Set active class on clicked button
+      page = e.target.getAttribute('href');
+      showPage(studentList, page);
+      e.target.setAttribute('class', 'active');
+    });
+
+    listItem.append(button);
+    pageLinks.append(listItem);
+
+    if(i === page) {
+      button.setAttribute('class', 'active');
+    }
+  }
+  // Append pagination links to DOM
+  pageContainer.append(pageLinksContainer);
+}
